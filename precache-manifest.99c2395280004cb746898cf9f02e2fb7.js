5d174d41347bbef071e90edfa83e08ca71187068
Ibrahim El Mountasser <mountasser@Ibrahims-MacBook-Pro.local>
2021-02-26 17:58:45 +0100

    Updates

---
+++ precache-manifest.99c2395280004cb746898cf9f02e2fb7.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afd0c3759e647b3a69ac0881cb2206ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1827c8104ce102a850b2",
+    "url": "/static/css/7.de3834ea.chunk.css"
+  },
+  {
+    "revision": "d11ec2063f5ee600f9d7",
+    "url": "/static/css/main.3bd0156a.chunk.css"
+  },
+  {
+    "revision": "4ccfd10f5ec8abf8240f",
+    "url": "/static/js/0.f1fc9223.chunk.js"
+  },
+  {
+    "revision": "a05238e7cc801b15eb98",
+    "url": "/static/js/1.7af18b88.chunk.js"
+  },
+  {
+    "revision": "d098344d5de1b8839db9",
+    "url": "/static/js/10.1dd1732c.chunk.js"
+  },
+  {
+    "revision": "54d18ed968f20e0005b1",
+    "url": "/static/js/11.e5a8cc97.chunk.js"
+  },
+  {
+    "revision": "4d82f5cd88dd771f68f8",
+    "url": "/static/js/12.734c74e1.chunk.js"
+  },
+  {
+    "revision": "a0d0da766846904b6e73",
+    "url": "/static/js/13.262a8855.chunk.js"
+  },
+  {
+    "revision": "87740887061709dcab53",
+    "url": "/static/js/14.833f61ff.chunk.js"
+  },
+  {
+    "revision": "15cf52530f1b1f66cea2",
+    "url": "/static/js/15.6a830048.chunk.js"
+  },
+  {
+    "revision": "874db5bf86175539c103",
+    "url": "/static/js/16.1b594fd9.chunk.js"
+  },
+  {
+    "revision": "e7152063b1f939909d00",
+    "url": "/static/js/17.d88aebba.chunk.js"
+  },
+  {
+    "revision": "a93aea257b14185bcb08",
+    "url": "/static/js/18.e8735724.chunk.js"
+  },
+  {
+    "revision": "4d254c9b956ff2a8744d",
+    "url": "/static/js/19.f06f9450.chunk.js"
+  },
+  {
+    "revision": "5aecd517a5ec6629c043",
+    "url": "/static/js/2.66e3d3e0.chunk.js"
+  },
+  {
+    "revision": "cdced331a6e3e16dba9b",
+    "url": "/static/js/20.b6a0e3a9.chunk.js"
+  },
+  {
+    "revision": "ce9f1dac349559433527",
+    "url": "/static/js/21.aebf1cb6.chunk.js"
+  },
+  {
+    "revision": "f9725a9458368a021395",
+    "url": "/static/js/22.71628ab3.chunk.js"
+  },
+  {
+    "revision": "3ee0b6ee6f0cf1406116",
+    "url": "/static/js/23.7cef872c.chunk.js"
+  },
+  {
+    "revision": "f327a713fee7cb057366",
+    "url": "/static/js/3.2343cc0b.chunk.js"
+  },
+  {
+    "revision": "46b764134a52a6b09c60",
+    "url": "/static/js/4.ad2cbfef.chunk.js"
+  },
+  {
+    "revision": "1827c8104ce102a850b2",
+    "url": "/static/js/7.afe9afad.chunk.js"
+  },
+  {
+    "revision": "c77eb71664fba0eee0e8",
+    "url": "/static/js/8.508d826b.chunk.js"
+  },
+  {
+    "revision": "978ead9202e4c770eba1",
+    "url": "/static/js/9.2bdd65b7.chunk.js"
+  },
+  {
+    "revision": "d11ec2063f5ee600f9d7",
+    "url": "/static/js/main.456dd899.chunk.js"
+  },
+  {
+    "revision": "a3287e6450e5cae86fcb",
+    "url": "/static/js/runtime-main.4846f0db.js"
+  },
+  {
+    "revision": "01798bc13e33afc36a52f2826638d386",
+    "url": "/static/media/Pe-icon-7-stroke.01798bc1.ttf"
+  },
+  {
+    "revision": "71394c0c7ad6c1e7d5c77e8ac292fba5",
+    "url": "/static/media/Pe-icon-7-stroke.71394c0c.eot"
+  },
+  {
+    "revision": "b38ef310874bdd008ac14ef3db939032",
+    "url": "/static/media/Pe-icon-7-stroke.b38ef310.woff"
+  },
+  {
+    "revision": "dedf26989fdd78c33cb9ae664a604d6c",
+    "url": "/static/media/Pe-icon-7-stroke.dedf2698.svg"
+  },
+  {
+    "revision": "32400f4e08932a94d8bfd2422702c446",
+    "url": "/static/media/fontawesome-webfont.32400f4e.eot"
+  },
+  {
+    "revision": "65e8f7dd6a65aa5812afa9050bfc720e",
+    "url": "/static/media/fontawesome-webfont.65e8f7dd.svg"
+  },
+  {
+    "revision": "a35720c2fed2c7f043bc7e4ffb45e073",
+    "url": "/static/media/fontawesome-webfont.a35720c2.woff"
+  },
+  {
+    "revision": "a3de2170e4e9df77161ea5d3f31b2668",
+    "url": "/static/media/fontawesome-webfont.a3de2170.ttf"
+  },
+  {
+    "revision": "db812d8a70a4e88e888744c1c9a27e89",
+    "url": "/static/media/fontawesome-webfont.db812d8a.woff2"
+  },
+  {
+    "revision": "1fbfd4bcffccb94e8e8a5ea70616b296",
+    "url": "/static/media/lg.1fbfd4bc.woff"
+  },
+  {
+    "revision": "2ec2cb2199d4d881e6a6ad86690f6add",
+    "url": "/static/media/lg.2ec2cb21.svg"
+  },
+  {
+    "revision": "f4292655f93dd12d9b8e4fc067ef2489",
+    "url": "/static/media/lg.f4292655.ttf"
+  }
+]);
